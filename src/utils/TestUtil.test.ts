da3be562bb40f495e30e13f09a13328674238991
Michael Squitieri <michael.squitieri@gmail.com>
2020-10-14 18:23:45 +0200

    remove unneeded test controllers.

--- src/utils/TestUtil.test.ts
+++
@@ -1,9 +0,0 @@
-import TestUtil from './TestUtil';
-
-describe('TestUtil', () => {
-  describe('getTest', () => {
-    test('returns test', () => {
-      expect(TestUtil.getTest()).toEqual('test');
-    });
-  });
-});
--- src/utils/TestUtil.ts
+++
@@ -1,5 +0,0 @@
-export default class TestUtil {
-  static getTest(): string {
-    return 'test';
-  }
-}
