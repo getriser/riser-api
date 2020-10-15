f2a7141a80e827c8e0f85123ecf32daa96b57138
Michael Squitieri <michael.squitieri@gmail.com>
2020-10-15 20:44:43 +0200

    add UserController.test.ts and return a token.

---
+++ src/controller/UserController.test.ts
@@ -0,0 +1,44 @@
+import AuthService from '../services/AuthService';
+
+const supertest = require('supertest');
+import { app } from '../app';
+import { RegisterUserProperties } from '../types';
+import ConnectionUtil from '../test-utils/ConnectionUtil';
+import UserService from '../services/UserService';
+const request = supertest(app);
+
+describe('UserController', () => {
+  beforeAll(async () => {
+    await ConnectionUtil.connect();
+  });
+
+  afterAll(async () => {
+    await ConnectionUtil.disconnect();
+  });
+
+  describe('register', () => {
+    it('registers a user', async (done) => {
+      const registerParams: RegisterUserProperties = {
+        email: 'blah@blah.com',
+        password: 'abcd1234',
+        passwordConfirmation: 'abcd1234',
+      };
+
+      const response = await request
+        .post('/user/register')
+        .send(registerParams);
+
+      expect(response.status).toEqual(200);
+
+      const user = await UserService.getUser(response.body.id);
+
+      expect(response.body.id).toEqual(user.id);
+      expect(response.body.email).toEqual(user.email);
+
+      const jwt = await AuthService.verifyToken(response.body.token);
+      expect(jwt.userId).toEqual(user.id);
+
+      done();
+    });
+  });
+});
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,23 +1,27 @@
 import { Controller, Route, Post, Body } from 'tsoa';
 import { RegisterUserProperties } from '../types';
 import UserService from '../services/UserService';
+import AuthService from '../services/AuthService';
 
 interface RegisterResponse {
   id: number;
   email: string;
+  token: string;
 }
 
 @Route('user')
-export class AuthController extends Controller {
+export class UserController extends Controller {
   @Post('register')
   public async register(
     @Body() body: RegisterUserProperties
   ): Promise<RegisterResponse> {
     const user = await UserService.registerUser(body);
+    const token = await AuthService.generateToken(user);
 
     return {
       id: user.id,
       email: user.email,
+      token,
     };
   }
 }
