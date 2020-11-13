db4e87ba441e48cc8aea4d4f1d0ced56842e09fd
Michael Squitieri <michael.squitieri@gmail.com>
2020-11-13 19:02:27 +0200

    integrate dotenv.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 interface Config {
   jwtSecret: string;
   assetPath: string;
 }
 
 const config: Config = {
-  jwtSecret: 'this-is-a-jwt-secret',
-  assetPath: 'http://localhost:3000',
+  jwtSecret: process.env.EXPRESS_APP_JWT_SECRET,
+  assetPath: process.env.EXPRESS_APP_ASSET_PATH,
 };
 
 export default config;
